112bf3d8d9cb8ef3b19ef3726b9e233ac5d8cdfb
shahad-FS <alsaneea.s@gmail.com>
2025-10-31 08:25:42 +0000

    update the code format to follow betty style

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -12,17 +12,17 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 
 	if (tree == NULL || tree->left == NULL)
 		return (NULL);
-	
+
 	pivot = tree->left;
 	tmp = pivot->right;
 	pivot->right = tree;
 	tree->left = tmp;
-	if(tmp != NULL)
+	if (tmp != NULL)
 		tmp->parent = tree;
 	tmp = tree->parent;
 	tree->parent = pivot;
 	pivot->parent = tmp;
-	if(tmp != NULL)
+	if (tmp != NULL)
 	{
 		if (tmp->left == tree)
 			tmp->left = pivot;
