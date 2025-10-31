c7f2f7f23be9a1199ee28a23c1055bde5cdd9eb7
shahad-FS <alsaneea.s@gmail.com>
2025-10-31 09:45:42 +0000

    Update the code format to follow betty style

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_levelorder - Goes through a binary tree using level-order traversal
+ * binary_tree_levelorder - Goes through a binary tree
+ * using level-order traversal
  * @tree: Pointer to the root node of the tree
  * @func: Pointer to function to call for each node
  *
@@ -19,20 +20,19 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	queue = malloc(sizeof(binary_tree_t *) * size);
 	if (!queue)
 		return;
-	
 	start = 0;
 	end = 0;
 	queue[end++] = tree;
-
 	while (start < end)
-		{
-			const binary_tree_t *node = queue[start++];
-			func(node->n);
+	{
+		const binary_tree_t *node = queue[start++];
 
-			if (node->left)
-				queue[end++] = node->left;
-			if (node->right)
-				queue[end++] = node->right;
+		func(node->n);
+
+		if (node->left)
+			queue[end++] = node->left;
+		if (node->right)
+			queue[end++] = node->right;
 		}
 	free(queue);
 }
