bffe3e2ad666fd4e706efd268da12889d07f9422
shahad-FS <alsaneea.s@gmail.com>
2025-10-31 09:58:06 +0000

    Update the code format to follow betty style

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -14,8 +14,8 @@ int is_bst_helper(const binary_tree_t *tree, int min, int max)
 	{
 		if (tree->n < min || tree->n > max)
 			return (0);
-	       return (is_bst_helper(tree->left, min, tree->n - 1) &&
-		       is_bst_helper(tree->right, tree->n + 1, max));
+		return (is_bst_helper(tree->left, min, tree->n - 1) &&
+		is_bst_helper(tree->right, tree->n + 1, max));
 	}
 	return (1);
 }
